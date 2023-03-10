fbbe6d3fa6d85b35126c65544f3f213cf60fa114
tiagobluiz <tiagoluiz@tecnico.ulisboa.pt>
2023-03-10 22:16:49 +0000

    Add index on station status and fix some errors on SQL definition when taking into account Postgres dialect

--- db/init.sql
+++ db/init.sql
@@ -5,14 +5,16 @@ CREATE TABLE IF NOT EXISTS Station (
     parish varchar(256) NOT NULL,
     capacity smallint NOT NULL,
     status varchar(50) NOT NULL, -- PLANNED / INSTALLING / TESTING / INACTIVE / ACTIVE
-    electricBikes smallint NULL,
-    classicBikes smallint NULL,
-    ADD CONSTRAINT UK_coordinates UNIQUE(coordinates)
+    electricBikes smallint NOT NULL DEFAULT 0,
+    classicBikes smallint NOT NULL DEFAULT 0,
+    CONSTRAINT UK_address UNIQUE(address)
 );
+
+CREATE INDEX IF NOT EXISTS idx_station_status ON Station(status);
 
 CREATE TABLE IF NOT EXISTS Bike (
     id serial PRIMARY KEY,
-    'type' varchar(50) NOT NULL, -- ELECTRIC / CLASSIC
+    type varchar(50) NOT NULL, -- ELECTRIC / CLASSIC
     status varchar(50) NOT NULL, -- ON_USE / AVAILABLE / ON_MAINTENANCE / REQUIRING_MAINTENANCE
     km int NOT NULL DEFAULT 0,
     last_maintenance_date date NULL
@@ -22,11 +24,11 @@ CREATE TABLE IF NOT EXISTS Trip(
     id bigserial PRIMARY KEY,
     departure_station int NOT NULL,
     arrival_station int NOT NULL,
-    bike_id itn NOT NULL,
+    bike_id int NOT NULL,
     travel_time time NOT NULL,
-    ADD CONSTRAINT FK_Trip_DepartureStation FOREIGN KEY (departure_station) REFERENCES Station(id),
-    ADD CONSTRAINT FK_Trip_ArrivalStation FOREIGN KEY (arrival_station) REFERENCES Station(id),
-    ADD CONSTRAINT FK_Trip_Bike FOREIGN KEY (bike_id) REFERENCES Bike(id)
+    CONSTRAINT FK_Trip_DepartureStation FOREIGN KEY (departure_station) REFERENCES Station(id),
+    CONSTRAINT FK_Trip_ArrivalStation FOREIGN KEY (arrival_station) REFERENCES Station(id),
+    CONSTRAINT FK_Trip_Bike FOREIGN KEY (bike_id) REFERENCES Bike(id)
 );
 
 CREATE TABLE IF NOT EXISTS UserBikeReview (
@@ -34,5 +36,5 @@ CREATE TABLE IF NOT EXISTS UserBikeReview (
     stars smallint NOT NULL,
     review varchar(512) NULL,
     target_bike_id int NOT NULL,
-    ADD CONSTRAINT FK_UserReview_Bike FOREIGN KEY (target_bike_id) REFERENCES Bike(id)
+    CONSTRAINT FK_UserReview_Bike FOREIGN KEY (target_bike_id) REFERENCES Bike(id)
 );
