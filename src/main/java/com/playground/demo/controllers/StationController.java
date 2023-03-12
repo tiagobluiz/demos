35ddf29c477b2e8e6bfcc5b14f8342b298f8d70a
tiagobluiz <tiagoluiz@tecnico.ulisboa.pt>
2023-03-12 14:21:02 +0000

    Remove GiraClient - in the new scope of this project it makes no sense as the aim is at replacing it, not using it.

--- src/main/java/com/playground/demo/controllers/StationController.java
+++ src/main/java/com/playground/demo/controllers/StationController.java
@@ -1,25 +1,14 @@
 package com.playground.demo.controllers;
 
-import com.playground.demo.models.Station;
 import com.playground.demo.services.StationService;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/stations")
+@RequiredArgsConstructor
 public class StationController {
 
     private final StationService stationService;
-
-    public StationController(StationService stationService) {
-        this.stationService = stationService;
-    }
-
-    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Station getStation(@PathVariable int id) {
-        return stationService.getStation(id);
-    }
 }
