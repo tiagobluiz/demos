10265f3860d66acc5324d7350fe381a182d68914
Tiago Luiz <tiagobluiz.97@gmail.com>
2023-05-05 10:39:27 +0100

    Add support to update bikes. Integration tests were also added. (#25)

--- src/test/java/com/playground/demo/controllers/BikeControllerIntegrationTest.java
+++ src/test/java/com/playground/demo/controllers/BikeControllerIntegrationTest.java
@@ -5,9 +5,7 @@ import com.playground.demo.exceptions.ExceptionalResponse;
 import com.playground.demo.models.BikeModel;
 import com.playground.demo.models.BikeRequest;
 import com.playground.demo.models.enums.AssetStatus;
-import com.playground.demo.models.enums.BikeType;
 import com.playground.demo.utils.PostgisSQLContainerInitializer;
-import com.playground.demo.utils.TestUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -27,7 +25,13 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import java.io.IOException;
 import java.time.LocalDate;
 
+import static com.playground.demo.models.enums.AssetStatus.ACTIVE;
+import static com.playground.demo.models.enums.AssetStatus.INACTIVE;
+import static com.playground.demo.models.enums.BikeType.CLASSIC;
+import static com.playground.demo.models.enums.BikeType.ELECTRIC;
+import static com.playground.demo.utils.TestUtils.readFileAsString;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpMethod.PUT;
 import static org.springframework.http.HttpStatus.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
@@ -57,8 +61,8 @@ class BikeControllerIntegrationTest {
 
         final var expectedResponse = BikeModel.builder()
                 .id(111)
-                .type(BikeType.ELECTRIC)
-                .status(AssetStatus.ACTIVE)
+                .type(ELECTRIC)
+                .status(ACTIVE)
                 .kms(555)
                 .lastMaintenanceDate(LocalDate.of(2022, 3, 15))
                 .build();
@@ -79,13 +83,11 @@ class BikeControllerIntegrationTest {
         assertThat(response.getStatusCode()).isEqualTo(NOT_FOUND);
     }
 
-    @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "/db/init.sql")
-    @Sql(executionPhase = AFTER_TEST_METHOD, scripts = "/db/clean.sql")
     @Test
     void givenValidArguments_whenCreatingABike_thenBikeIsPersisted() {
         // given
         final var bikeRequest = BikeRequest.builder()
-                .type(BikeType.ELECTRIC)
+                .type(ELECTRIC)
                 .status(AssetStatus.INACTIVE)
                 .kms(123456798) // should be ignored as all bikes start with 0
                 .build();
@@ -122,12 +124,11 @@ class BikeControllerIntegrationTest {
     @ParameterizedTest
     @ValueSource(strings = {
             "bikeController/bikeRequest_invalidStatus.json",
-            "bikeController/bikeRequest_invalidType.json",
-            "bikeController/bikeRequest_invalidKms.json"
+            "bikeController/bikeRequest_invalidType.json"
     })
     void givenInvalidArguments_whenCreatingABike_thenBadRequestIsReturned(final String filename) throws IOException {
         // given
-        final var input = TestUtils.readFileAsString(this.getClass(), filename);
+        final var input = readFileAsString(this.getClass(), filename);
 
         final var headers = new HttpHeaders();
         headers.setContentType(APPLICATION_JSON);
@@ -149,13 +150,93 @@ class BikeControllerIntegrationTest {
     @Sql(executionPhase = AFTER_TEST_METHOD, scripts = "/db/clean.sql")
     @Test
     void givenValidArguments_whenUpdatingABike_thenUpdatedBikeIsPersisted() {
+        // given
+        final var input = BikeRequest.builder()
+                .type(CLASSIC)
+                .status(INACTIVE)
+                .kms(123456)
+                .build();
 
+        final var headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_JSON);
+
+        final var entity = new HttpEntity<>(input, headers);
+
+        // when
+        final var updateResponse = restTemplate.exchange("/bikes/111", PUT, entity, BikeModel.class);
+
+        // then
+        assertThat(updateResponse).isNotNull();
+        assertThat(updateResponse.getStatusCode()).isEqualTo(OK);
+        assertThat(updateResponse.getBody()).isNotNull();
+
+        final var updatedBike = updateResponse.getBody();
+
+        assertThat(updatedBike)
+                .extracting(BikeModel::getId, BikeModel::getType, BikeModel::getStatus, BikeModel::getKms)
+                .containsExactly(111, input.getType(), input.getStatus(), input.getKms());
+
+        // verify it was persisted
+        final var response = restTemplate.getForEntity("/bikes/111", BikeModel.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(OK);
+        assertThat(response.getBody())
+                .isNotNull()
+                .isEqualTo(updatedBike);
     }
 
-    @Sql(executionPhase = BEFORE_TEST_METHOD, scripts = "/db/init.sql")
-    @Sql(executionPhase = AFTER_TEST_METHOD, scripts = "/db/clean.sql")
     @Test
-    void givenInvalidArguments_whenUpdatingABike_thenBadRequestIsReturned() {
+    void givenNonexistentBike_whenUpdatingABike_thenNotFoundIsReturned() {
+        // given
+        final var input = BikeRequest.builder()
+                .type(ELECTRIC)
+                .status(ACTIVE)
+                .kms(123456)
+                .build();
 
+        final var headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_JSON);
+
+        final var entity = new HttpEntity<>(input, headers);
+
+        // when
+        final var response = restTemplate.exchange("/bikes/999", PUT, entity, ExceptionalResponse.class);
+
+        // then
+        final var expectedResponse = ExceptionalResponse.builder()
+                .reason("The requested bike does not exist, please verify that the identifier is correct.")
+                .faultyValue("id", 999)
+                .build();
+
+        assertThat(response.getStatusCode()).isEqualTo(NOT_FOUND);
+        assertThat(response.getBody())
+                .isNotNull()
+                .isEqualTo(expectedResponse);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "bikeController/bikeRequest_invalidStatus.json",
+            "bikeController/bikeRequest_invalidType.json",
+            "bikeController/bikeRequest_invalidKms.json"
+    })
+    void givenInvalidArguments_whenUpdatingABike_thenBadRequestIsReturned(final String filename) throws IOException {
+        // given
+        final var input = readFileAsString(this.getClass(), filename);
+
+        final var headers = new HttpHeaders();
+        headers.setContentType(APPLICATION_JSON);
+
+        final var entity = new HttpEntity<>(input, headers);
+
+        // when
+        final var response = restTemplate.exchange("/bikes/111", PUT, entity, ExceptionalResponse.class);
+
+        // then
+        assertThat(response.getStatusCode()).isEqualTo(BAD_REQUEST);
+        assertThat(response.getBody())
+                .isNotNull()
+                .extracting(ExceptionalResponse::getReason)
+                .isEqualTo("The request was badly formed! Please verify that the indicated field is using the accepted values.");
     }
 }
